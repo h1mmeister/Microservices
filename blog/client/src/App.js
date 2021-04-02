74857a49a9ba5f047a4ab63dd692c0bdc412a437
h1m <himanshu.yadav643@gmail.com>
2021-04-02 07:37:59 +0530

    completed postcreate and postlist components

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
 const App = () => {
-  return <div>Blog Post</div>;
+  return (
+    <div className="container">
+      <h2>Create Post</h2>
+      <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
---
+++ blog/client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState(""); // the is initial state
+
+  const onSubmit = async (event) => {
+    // this is the post request to our /posts service
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+    setTitle(""); // this would blank out the input after submission
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary" style={{ marginTop: 10 }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
