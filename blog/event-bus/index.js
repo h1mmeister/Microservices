3f75e0d4150292702d9db6e0f0ff855ac4854c23
h1m <himanshu.yadav643@gmail.com>
2021-05-04 10:21:31 +0530

    app completed, moving to deployment

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,14 +5,21 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
 // Emitting events to listeners
 app.post("/events", (req, res) => {
   const event = req.body;
+  events.push(event);
   axios.post("http://localhost:4000/events", event); // post service
   axios.post("http://localhost:4001/events", event); // comment service
   axios.post("http://localhost:4002/events", event); // query service
   axios.post("http://localhost:4003/events", event); // moderation service
   res.send({ status: "OK" });
+});
+
+// To get all the events
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
