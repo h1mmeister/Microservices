063e64d6d75e9363d48810ff30f1c11757b333bb
h1m <himanshu.yadav643@gmail.com>
2021-05-24 14:51:27 +0530

    added deployment files for all the services

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -10,10 +10,10 @@ const events = [];
 app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event);
-  axios.post("http://localhost:4000/events", event); // post service
-  axios.post("http://localhost:4001/events", event); // comment service
-  axios.post("http://localhost:4002/events", event); // query service
-  axios.post("http://localhost:4003/events", event); // moderation service
+  axios.post("http://posts-clusterip-srv:4000/events", event); // post service
+  // axios.post("http://localhost:4001/events", event); // comment service
+  // axios.post("http://localhost:4002/events", event); // query service
+  // axios.post("http://localhost:4003/events", event); // moderation service
   res.send({ status: "OK" });
 });
 
